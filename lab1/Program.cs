64f3db7db42e58c40fb8fdf8a5f7807342382511
natural01 <80067153+natural01@users.noreply.github.com>
2022-03-23 20:26:10 +0300

    Add files via upload

---
+++ lab1/Program.cs
@@ -0,0 +1,47 @@
+﻿ using System;
+
+    namespace Calculator
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                float num1 = 0; 
+                float num2 = 0;
+
+                Console.WriteLine("Type a number, and then press Enter");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Type another number, and then press Enter");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Choose an option from the following list:");
+                Console.WriteLine("\t1 - Add");
+                Console.WriteLine("\t2 - Subtract");
+                Console.WriteLine("\t3 - Multiply");
+                Console.WriteLine("\t4 - Divide");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                        break;
+                    case "2":
+                        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                        break;
+                    case "3":
+                        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                        break;
+                    case "4":
+                        while (num2 == 0)
+                        {
+                            Console.WriteLine("Enter a non-zero divisor: ");
+                            num2 = Convert.ToInt32(Console.ReadLine());
+                        }
+                        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                        break;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
